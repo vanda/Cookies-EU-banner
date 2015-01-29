f50d9cf54cf0334bf1b35287840345616bf0d654
Alex-D <contact@alex-d.fr>
2015-01-29 17:58:45 +0100

    Init GH page

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,24 +3,7 @@ var gulp = require('gulp'),
     vinylPaths = require('vinyl-paths'),
     $ = require('gulp-load-plugins')();
 
-var pkg = require('./package.json');
-var banner = ['/**',
-    ' * <%= pkg.title %> v<%= pkg.version %> - <%= pkg.description %>',
-    ' * ------------------------',
-    ' * @link <%= pkg.homepage %>',
-    ' * @license <%= pkg.license %>',
-    ' * @author <%= pkg.author.name %>',
-    ' *         Twitter : @AlexandreDemode',
-    ' *         Website : <%= pkg.author.url.replace("http://", "") %>',
-    ' */',
-    '\n'].join('\n');
-var bannerLight = ['/** <%= pkg.title %> v<%= pkg.version %> - <%= pkg.description %>',
-    ' - <%= pkg.homepage.replace("http://", "") %>',
-    ' - License <%= pkg.license %>',
-    ' - Author : <%= pkg.author.name %>',
-    ' / <%= pkg.author.url.replace("http://", "") %>',
-    ' */',
-    '\n'].join('');
+var mainStyle = 'scss/main.scss';
 
 
 
@@ -30,36 +13,29 @@ gulp.task('clean', function(){
         .pipe(vinylPaths(del));
 });
 
-gulp.task('test', function(){
-    return gulp.src(['cookies-eu-banner.js'])
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('scripts', ['test'], function(){
-    return gulp.src(['cookies-eu-banner.js'])
-        .pipe($.header(banner, { pkg: pkg }))
-        .pipe($.concat('cookies-eu-banner.js', { newLine: '\r\n\r\n' }))
-        .pipe($.size({ title: 'cookies-eu-banner.js' }))
-        .pipe($.rename({ suffix: ".min" }))
-        .pipe($.uglify())
-        .pipe($.header(bannerLight, { pkg: pkg }))
-        .pipe(gulp.dest('.'))
-        .pipe($.size({ title: 'cookies-eu-banner.min.js' }))
+gulp.task("styles", function(){
+  return gulp.src(mainStyle)
+    .pipe($.sass({
+      sass: 'sass',
+      includePaths: ['sass']
+    }))
+    .pipe($.autoprefixer(["last 1 version", "> 1%", "ff >= 20", "ie >= 8", "opera >= 12", "Android >= 2.2"], { cascade: true }))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest("css/"))
 });
 
 
 
 gulp.task('watch', function(){
-    gulp.watch(['cookies-eu-banner.js'], ['scripts']);
+    gulp.watch(mainStyle, ['styles']);
 
-    gulp.watch(['/*.min.js'], function(file){
+    gulp.watch(['css/**', 'img/*', 'js/*'], function(file){
         $.livereload.changed(file);
     });
 
     $.livereload.listen();
 });
 
-gulp.task('build', ['scripts']);
+gulp.task('build', ['styles']);
 
 gulp.task('default', ['build', 'watch']);
